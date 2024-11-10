db9ac616e1df8c7b557d1d5e577b2ffaccd6a538
RoStormCreations <rostormstudios@gmail.com>
2024-11-10 17:08:40 -0500

    yes

--- app.js
+++ app.js
@@ -35,16 +35,29 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
     const { name } = data;
 
     // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
-        },
-      });
-    }
+// "test" command
+if (name === 'test') {
+    // Send a message into the channel where command was triggered from
+    return res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+        // Fetches a random emoji to send from a helper function
+        content: 'hello world ' + getRandomEmoji(),
+    },
+    });
+}// "test" command
+// "test" command
+// "test" command
+if (name === 'test') {
+    // Send a message into the channel where command was triggered from
+    return res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+        // Fetches a random emoji to send from a helper function
+        content: 'hello world ' + getRandomEmoji(),
+    },
+    });
+}
 
     console.error(`unknown command: ${name}`);
     return res.status(400).json({ error: 'unknown command' });
